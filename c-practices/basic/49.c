271eb74c6e4e7c7c31abb4fa5cdf6f381247a412
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2026-01-30 23:29:34 +0600

    update

---
+++ c-practices/basic/49.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int x, y = 1;
+    printf("Enter a number\t:");
+    scanf("%d", &x);
+    int arr[7];
+
+    for(int i = 0; i<7; i++){
+        arr[i] = 
+    }
+}
