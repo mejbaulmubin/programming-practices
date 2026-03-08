8fc8daae9dc1b216a9bcbb8510da3f2ad8543449
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2026-03-08 22:01:38 +0600

    update 03 in data structures.

---
+++ c-practices/w3resource/Control Flow/Data Structures/03.c
@@ -0,0 +1,13 @@
+/*
+3. Array Sum Calculation
+
+Write a program in C to find the sum of all elements of the array.
+Test Data :
+Input the number of elements to be stored in the array :3
+Input 3 elements in the array :
+element - 0 : 2
+element - 1 : 5
+element - 2 : 8
+Expected Output :
+Sum of all elements stored in the array is : 15
+*/
